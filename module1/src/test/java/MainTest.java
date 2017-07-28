f13016a5638ea5dbc69edbdfd31e1831c1c93501
Abdellatif El Maknati <abdellatif.elmaknati@gmail.com>
2017-07-28 15:15:13 +0200

    Initial commit

---
+++ module1/src/test/java/MainTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by EL MAKNATI on 28/07/2017.
+ */
+public class MainTest {
+
+    @Test
+    public void test1(){
+        assertEquals(5 , 5);
+    }
+
+}
---
+++ module2/src/main/java/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by EL MAKNATI on 28/07/2017.
+ */
+public class Main {
+    /**
+     *
+     * @param args Arguments
+     */
+    public static void main(String args[]){
+        System.out.println("ok module 2");
+    }
+}
